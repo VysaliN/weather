e00b636a9a2fed6f4dc29f39ff20b10fd9e342f8
Vysali Natarajan Jayaraman <vjnatarajan@AD.MSYSTECHNOLOGIES.COM>
2023-01-27 21:41:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Weather from "./Weather";
+import "./Weather.css";
+
+const App = () => {
+  return (
+    <div>
+      <center>
+        <Weather />
+      </center>
+    </div>
+  );
+};
+
+export default App;
